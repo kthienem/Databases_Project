2f414f3a1bd30ecadea2c8c435c025ec4022cf09
Patrick Ryan Drumm <pdrumm@dsg1.virtual.crc.nd.edu>
2015-10-29 21:05:52 -0400

    Test push

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<html><!--test push-->
+<html><!--test push!-->
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
