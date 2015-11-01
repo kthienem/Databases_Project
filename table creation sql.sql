c9950657bbced35410dd0dd958708da6d66e1286
pdrumm <pdrumm@nd.edu>
2015-11-01 17:15:07 -0500

    final changes to database architecture

--- table creation sql.sql
+++ table creation sql.sql
@@ -12,11 +12,11 @@ CREATE TABLE Location(
 );
 
 CREATE TABLE Tourn_Loc_Junc(
-	tourn_ID INT NOT NULL AUTO_INCREMENT,
+	tourn_ID INT NOT NULL,
 	loc_ID INT NOT NULL,
 	PRIMARY KEY ( tourn_ID, loc_ID ),
-	FOREIGN KEY ( tourn_ID ) REFERENCES Tournament ( tourn_ID ),
-	FOREIGN KEY ( loc_ID ) REFERENCES Location ( loc_ID )
+	FOREIGN KEY ( tourn_ID ) REFERENCES Tournament ( tourn_ID ) ON UPDATE CASCADE,
+	FOREIGN KEY ( loc_ID ) REFERENCES Location ( loc_ID ) ON UPDATE CASCADE
 );
 
 CREATE TABLE Team(
@@ -26,27 +26,23 @@ CREATE TABLE Team(
 	bracket_position INT,
 	tourn_ID INT,
 	PRIMARY KEY ( team_ID ),
-	FOREIGN KEY ( tourn_ID ) REFERENCES Tournament ( tourn_ID )
+	FOREIGN KEY ( tourn_ID ) REFERENCES Tournament ( tourn_ID ) ON UPDATE CASCADE
 );
 
-# Potential flaw!
-# should we remove loc_ID as part of the primary key and add team1, team2?
-# logic -> there may be a whole bunch of games at once that do not have a location (aka location=other)
-# However there will never be more than one game at a time in which a given team will be playing in
-# 
-# Additionally, make sure that Game_Team_Junc is not redundant
-
 CREATE TABLE Game(
-	time DATETIME, 
+	time DATETIME NOT NULL, 
 	team1_ID INT,
 	team2_ID INT,
 	loc_ID INT,
 	tourn_ID INT,
-	PRIMARY KEY ( time, loc_ID ),
-	FOREIGN KEY ( team1_ID ) REFERENCES Team ( team_ID ),
-	FOREIGN KEY ( team2_ID ) REFERENCES Team ( team_ID ),
-	FOREIGN KEY ( loc_ID ) REFERENCES Location ( loc_ID ),
-	FOREIGN KEY ( tourn_ID ) REFERENCES Tournament ( tourn_ID )
+	points1 INT,
+	points2 INT,
+	team1_result BOOLEAN,
+	PRIMARY KEY ( time, team1_ID, team2_ID ),
+	FOREIGN KEY ( team1_ID ) REFERENCES Team ( team_ID ) ON UPDATE CASCADE,
+	FOREIGN KEY ( team2_ID ) REFERENCES Team ( team_ID ) ON UPDATE CASCADE,
+	FOREIGN KEY ( loc_ID ) REFERENCES Location ( loc_ID ) ON UPDATE CASCADE,
+	FOREIGN KEY ( tourn_ID ) REFERENCES Tournament ( tourn_ID ) ON UPDATE CASCADE
 );
 
 CREATE TABLE Player(
@@ -54,23 +50,21 @@ CREATE TABLE Player(
 	player_name VARCHAR(50),
 	email VARCHAR(50),
 	isCaptain BOOLEAN,
+	student_ID VARCHAR(20),
 	team_ID INT,
 	PRIMARY KEY ( player_ID ),
-	FOREIGN KEY ( team_ID ) REFERENCES Team ( team_ID )
+	FOREIGN KEY ( team_ID ) REFERENCES Team ( team_ID ) ON UPDATE CASCADE
 );
 
-CREATE TABLE Game_Team_Junc(
-	time DATETIME,
-	loc_ID INT,
-	team1_ID INT,
-	team2_ID INT,
-	PRIMARY KEY ( time, loc_ID, team1_ID, team2_ID ),
-	FOREIGN KEY ( time, loc_ID ) REFERENCES Game ( time, loc_ID ),
-	FOREIGN KEY 
-);
-
-CREATE TABLE Player_Team_Junc(
-	player_ID,
-	team_ID
-);
-
+#CREATE TABLE Game_Team_Junc(
+#	time DATETIME,
+#	team1_ID INT,
+#	team2_ID INT,
+#	PRIMARY KEY ( time, team1_ID, team2_ID ),
+#	FOREIGN KEY ( time ) REFERENCES Game ( time ),
+#	FOREIGN KEY 
+#);
+#CREATE TABLE Player_Team_Junc(
+#	player_ID,
+#	team_ID
+#);
